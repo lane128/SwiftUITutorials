77c6ef24db4d8ca55cd1c85c06d3558ff7ceb194
Adam <wanglei01@soulapp.cn>
2022-01-29 14:13:34 +0800

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Adam Wang on 2022/1/29.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
